fa5c61b274c7d6f14fe5687b450610149adcbd0f
uniquebby <15367145356@163.com>
2019-12-01 13:02:45 +0000

    finished project 3

--- src/concurrency/lock_manager.cpp
+++ src/concurrency/lock_manager.cpp
@@ -2,24 +2,115 @@
  * lock_manager.cpp
  */
 
+#include <algorithm>
+#include <cassert>
 #include "concurrency/lock_manager.h"
+using namespace std;
 
 namespace cmudb {
 
 bool LockManager::LockShared(Transaction *txn, const RID &rid) {
-  return false;
+  return GetLock(txn, rid, LockMode::SHARED);
 }
 
 bool LockManager::LockExclusive(Transaction *txn, const RID &rid) {
-  return false;
+  return GetLock(txn, rid, LockMode::EXCLUSIVE);
 }
 
 bool LockManager::LockUpgrade(Transaction *txn, const RID &rid) {
-  return false;
+  return GetLock(txn, rid, LockMode::UPGRADE);
 }
 
 bool LockManager::Unlock(Transaction *txn, const RID &rid) {
-  return false;
+  auto state = txn->GetState();
+  if (strict_2PL_) {
+    if (state != TransactionState::COMMITTED && state != TransactionState::ABORTED) {
+      txn->SetState(TransactionState::ABORTED);
+	  return false;
+    }
+  } else if (state == TransactionState::GROWING) {
+    txn->SetState(TransactionState::SHRINKING);
+  }
+  //1.get lock list
+  unique_lock<mutex> table_latch(mutex_);  
+  LockList &value = lock_table_[rid];
+  unique_lock<mutex> list_latch(value.mutex_);  
+  //2.find lock
+  auto iter = find_if(value.list_.begin(), value.list_.end(), 
+                      [txn] (const Lock &lock) { 
+					    return lock.txn_id_ == txn->GetTransactionId();
+					  });
+  assert(iter != value.list_.end()); 
+  //3.erase lock from lock list and transaction lock set.
+  
+  auto lock_set = iter->mode_ == LockMode::SHARED ? 
+                  txn->GetSharedLockSet() : txn->GetExclusiveLockSet(); 
+  assert(lock_set->erase(rid) == 1);
+  value.list_.erase(iter);
+  //4.bug forget erase rid if list is empty
+  if (value.list_.empty()) {
+    lock_table_.erase(rid);
+	return true;
+  }
+  table_latch.unlock();
+  //5.notify other passible waiting locks.
+  for (auto &lock : value.list_) {
+    //if have other granted lock, that's to say cur lock is shared lock
+	//and its unlock is no impact on other locks.
+    if (lock.is_granted_) 
+	  break;
+	lock.Grant();
+	if (lock.mode_ == LockMode::SHARED) {
+	  continue; 
+    }
+	if (lock.mode_ == LockMode::UPGRADE) {
+	  value.is_upgrading_ = false;
+	  lock.mode_ = LockMode::EXCLUSIVE;
+	}
+	break;
+  }
+  return true;
 }
+
+bool LockManager::GetLock(Transaction *txn, const RID &rid, LockMode mode) {
+  //assert mode is legal
+  if (txn->GetState() != TransactionState::GROWING) {
+    txn->SetState(TransactionState::ABORTED);
+    return false;
+  }
+  //get lock list
+  unique_lock<mutex> table_latch(mutex_);  
+  LockList &value = lock_table_[rid];
+  unique_lock<mutex> list_latch(value.mutex_);  
+  table_latch.unlock();
+  //special deal with upgrade mode 
+  if (mode == LockMode::UPGRADE) {
+    if (value.is_upgrading_) {
+      txn->SetState(TransactionState::ABORTED);
+	  return false;
+	}
+    auto iter = find_if(value.list_.begin(), value.list_.end(), 
+                        [txn] (const Lock &lock) { 
+	  				      return lock.txn_id_ == txn->GetTransactionId();
+					  });
+    if (iter == value.list_.end() || iter->mode_ != LockMode::SHARED
+        || !iter->is_granted_) {
+      txn->SetState(TransactionState::ABORTED);
+	  return false;
+    }
+    assert(txn->GetSharedLockSet()->erase(rid) == 1);
+    value.list_.erase(iter);
+  }
+  //insert to lock list
+  auto can_grant = value.CanGrant(mode);
+  //wait-die
+  if (!can_grant && value.list_.back().txn_id_ < txn->GetTransactionId()) {
+     txn->SetState(TransactionState::ABORTED);
+  	 return false;
+  }
+  value.Insert(txn, rid, mode, can_grant, &list_latch);
+  return true;
+}
+
 
 } // namespace cmudb
--- src/include/concurrency/lock_manager.h
+++ src/include/concurrency/lock_manager.h
@@ -17,7 +17,63 @@
 
 namespace cmudb {
 
+enum class LockMode { SHARED, EXCLUSIVE, UPGRADE }; 
+
 class LockManager {
+
+  struct Lock {
+    Lock (txn_id_t txn_id, bool is_granted, LockMode mode) 
+	    : txn_id_(txn_id), is_granted_(is_granted), mode_(mode) {}
+
+	void Wait() {
+	  std::unique_lock<std::mutex> ul(mutex_);
+	  cv_.wait(ul, [this] { return this->is_granted_; } );
+	}
+
+	void Grant() {
+	  std::lock_guard<std::mutex> lg(mutex_);
+	  is_granted_ = true;
+	  cv_.notify_one();
+	}
+	
+	std::mutex mutex_;
+	std::condition_variable cv_;
+	txn_id_t txn_id_;
+	bool is_granted_;
+	LockMode mode_;
+  };
+
+  struct LockList {
+    bool CanGrant(LockMode mode) {
+	  if (list_.empty()) return true;
+	  return (mode == LockMode::SHARED && 
+	          list_.back().mode_ == LockMode::SHARED && 
+			  list_.back().is_granted_);
+	}
+
+    void Insert(Transaction *txn, const RID &rid, LockMode mode,
+  	            bool can_grant, std::unique_lock<std::mutex> *lk) {
+      auto upgrading = (mode == LockMode::UPGRADE);
+	  if (upgrading && can_grant) {
+	    mode = LockMode::EXCLUSIVE;
+	  }
+	  list_.emplace_back(txn->GetTransactionId(), can_grant, mode);
+	  if (!can_grant) {
+	    is_upgrading_ |= upgrading;
+		lk->unlock();
+		list_.back().Wait();
+	  }
+      if (mode == LockMode::SHARED) {
+	    txn->GetSharedLockSet()->insert(rid);
+	  } else {
+	    txn->GetExclusiveLockSet()->insert(rid);
+	  }
+    }
+
+    std::mutex mutex_;
+    std::list<Lock> list_;
+	bool is_upgrading_ = false;
+  };
 
 public:
   LockManager(bool strict_2PL) : strict_2PL_(strict_2PL){};
@@ -38,7 +94,11 @@ public:
   /*** END OF APIs ***/
 
 private:
+  std::mutex mutex_;
   bool strict_2PL_;
+  std::unordered_map<RID, LockList> lock_table_;
+
+  bool GetLock(Transaction *txn, const RID &rid, LockMode mode);
 };
 
 } // namespace cmudb
